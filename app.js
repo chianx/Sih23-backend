0dae37b453a964306d07bfa7f5e24f381fc1b644
Chintan Grover <chinxgrover@gmail.com>
2023-09-23 14:16:46 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from  "express";
+import cors from "cors";
+import helmet from "helmet";
+import {db} from "./firebaseConfig/config.js"
+import {ref, onValue, set, update, get, remove, push} from "firebase/database";
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+
+app.post("/job", (req, res) => {
+    const body = req.body;
+    var jobRef = push(ref(db, "jobs"));
+    const job = {...body, jobId: jobRef.key};
+    set(jobRef, job).then (async() => {
+        console.log("Job posted sucessfully");
+    });
+    res.send({status : "200", msg:"Job addes successfully"})
+})
+
+app.get("/allJobs", (req, res) => {
+    
+    var jobRef = (ref(db, "jobs/"));
+    onValue(jobRef, async(snapshot) => {
+        const data = snapshot.val() != null ? snapshot.val() : [] ;
+        const allJobs = Object.keys(data).map(key => ({
+            ...data[key]
+        })).reverse()
+        res.send(allJobs);
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server is running at localhost 3000");
+})
---
+++ firebaseConfig/config.js
@@ -0,0 +1,19 @@
+import {initializeApp} from "firebase/app"
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBTRJUcTOjuTlPjo4BlBqUTOvJM2Y1-PMw",
+    authDomain: "pgrkam-683e5.firebaseapp.com",
+    databaseURL: "https://pgrkam-683e5-default-rtdb.firebaseio.com",
+    projectId: "pgrkam-683e5",
+    storageBucket: "pgrkam-683e5.appspot.com",
+    messagingSenderId: "339913483137",
+    appId: "1:339913483137:web:74c6ca174dcfb43f903e80",
+    measurementId: "G-N8G48H9MRG"
+};
+
+initializeApp(firebaseConfig);
+export const db = getDatabase();
+
+
+
